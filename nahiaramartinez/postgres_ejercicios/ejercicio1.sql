373627be21ea67b843ebc9277a8727aadb56757f
Nahiara martinez <martineznahiara311@gmail.com>
2023-09-25 17:02:43 -0300

    actividades

---
+++ nahiaramartinez/postgres_ejercicios/ejercicio1.sql
@@ -0,0 +1,9 @@
+INSERT INTO "user" (username,password)
+VALUES ('nahi','0311'),
+    ('facu','2022'),
+    ('angie','1234')
+
+INSERT INTO "post"(author_id,title,body)
+VALUES('1','RULO','TENGORULOS'),
+      ('2','FACU','MECHOCARON'),
+      ('3','ANGIE','MEGUSTATAYLOR');
